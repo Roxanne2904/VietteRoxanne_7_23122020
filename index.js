a35fe972b01bf055ace725aa605c1a77a7af1a1b
Roxanne <roxanne-viette@hotmail.fr>
2021-07-31 16:13:32 +0200

    mise en place du projet aves sass + eslint

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-let datas;
+// let datas;
 
 
-datas = recipes.map((data) => {
-    return data.ingredients.map((ingredient) => {
-        return ingredient.ingredient
-        console.log(ingredient.ingredient);
+// datas = recipes.map((data) => {
+//     return data.ingredients.map((ingredient) => {
+//         return ingredient.ingredient
+//         console.log(ingredient.ingredient);
+//     })
+// });
+// console.log(datas);
+let datas; 
+
+const fetchDatas = async() => {
+datas = await fetch("./datas/recipes.json")
+    .then((response) =>{
+        return response.json();
     })
+    .then((body)=>{
+        return body;
+    });
+};
+
+const showDatas = async ()=> {
+    await fetchDatas();
+    console.log(datas);
+};
+
+
+window.addEventListener("load", ()=>{
+    showDatas();
 });
-console.log(datas);
 
