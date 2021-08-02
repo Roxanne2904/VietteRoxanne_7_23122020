1d5d804282f55a2d02a1c4fe0aaa90549de973a0
Roxanne <roxanne-viette@hotmail.fr>
2021-08-02 15:55:13 +0200

    mis en place des données dynamiques des listes inputs

--- index.js
+++ index.js
@@ -24,21 +24,48 @@ const showDatasIntoInputs = async ()=> {
     await fetchDatas();
     // ----------------
     let dataIngredients;
+    let dataAppareils;
+    let dataUstensiles;
+    // ----------------
     let uniqueDataIngredients;
+    let uniqueDataAppareils;
+    let uniqueDataUstensiles;
+    // ----------------
     const ingredientsUl = document.getElementById("ingredients__ul");
+    const appareilsUl = document.getElementById("appareil__ul");
+    const ustensilesUl = document.getElementById("ustensiles__ul");
     // ----------------
     dataIngredients = datas.recipes.map((data)=>{
         return data.ingredients.map((ingredient)=>{
-            return ingredient.ingredient
-        })
-    }).flat();
+            return ingredient.ingredient;
+        });
+    })
+    .flat()
+    .sort((a, b) => a.localeCompare(b, "fr", {ignorePunctuation: true}));
+    // ----------------
+    dataAppareils = datas.recipes.map((data)=>{
+         return data.appliance;
+    }).sort();
+    // ----------------
+    dataUstensiles = datas.recipes.map((data)=>{
+        return data.ustensils;
+    })
+    .flat()
+    .sort((a, b) => a.localeCompare(b, "fr", {ignorePunctuation: true}));
     // ----------------
    uniqueDataIngredients = Array.from(new Set(dataIngredients));
-
+   uniqueDataAppareils = Array.from(new Set(dataAppareils));
+   uniqueDataUstensiles = Array.from(new Set(dataUstensiles));
+    // ----------------
    ingredientsUl.innerHTML = uniqueDataIngredients.map((ingredient)=>{
-     return `<li class="form__fieldset__bgd__content__ul__li">${ingredient}</li>`
+     return `<li class="form__fieldset__bgd__content__ul__li">${ingredient}</li>`;
    }).join("");
-    
+   appareilsUl.innerHTML = uniqueDataAppareils.map((appareil)=>{
+    return `<li class="form__fieldset__bgd__content__ul__li">${appareil}</li>`;
+  }).join("");
+  ustensilesUl.innerHTML = uniqueDataUstensiles.map((ustensil)=>{
+    return `<li class="form__fieldset__bgd__content__ul__li">${ustensil}</li>`;
+  }).join("");
 };
 
 
